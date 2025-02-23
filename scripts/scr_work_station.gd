a22e7487e0ba349fe78c93bc9fb2e9aacf200d83
aherrington7452 <aherrington7452@gmail.com>
2025-02-23 18:25:33 -0500

    update

--- scripts/scr_work_station.gd
+++ scripts/scr_work_station.gd
@@ -1,35 +1,43 @@
 extends Node2D
 
-# buttons have "default" and "selected" animations
-@onready var button_work: AnimatedSprite2D = $ButtonWork
-@onready var button_coffee: AnimatedSprite2D = $ButtonCoffee
-@onready var button_sleep: AnimatedSprite2D = $ButtonSleep
+@onready var state_label: AnimatedSprite2D = $StatesLabelOutline/StateLabel
+@onready var states_label_outline: AnimatedSprite2D = $StatesLabelOutline
 
 enum State { Work, Coffee, Sleep }
+
+var selection_state_dict = {0:"Work", 1:"Coffee", 2:"Sleep"}
 var current_state : State
+var selection_state 
 
 func _ready():
 	current_state = State.Work
+	selection_state = 0
 
-func _process(delta):
-	if Input.is_action_pressed("move_left"):
-		current_state = State.Coffee
-	elif Input.is_action_pressed("move_right"):
-		current_state = State.Sleep
-	elif Input.is_action_pressed("move_up"):
-		current_state = State.Work
-	button_animation()
+func _process(delta) -> void:
+	if Input.is_action_pressed("move_up"):
+		if selection_state < 2:
+			selection_state += 1
+		elif selection_state == 2:
+			selection_state = 0
+	print("Selection State: ", selection_state)
+	if Input.is_action_just_pressed("interact"):
+		current_state = selection_state
+	print("Current State ", current_state)
+"""
+	label_animation()
 
-func button_animation():
+func label_animation():
+	# TODO: if selection_state, then if current_state
 	if current_state == State.Work:
-		button_work.play("pressed")
-		button_coffee.play("default")
-		button_sleep.play("default")
+		state_label.play("pressed")
+		state_label.play("default")
+		state_label.play("default")
 	elif current_state == State.Coffee:
-		button_work.play("default")
-		button_coffee.play("pressed")
-		button_sleep.play("default")
+		state_label.play("default")
+		state_label.play("pressed")
+		state_label.play("default")
 	elif current_state == State.Sleep:
-		button_work.play("default")
-		button_coffee.play("default")
-		button_sleep.play("pressed")
+		state_label.play("default")
+		state_label.play("default")
+		state_label.play("pressed")
+"""

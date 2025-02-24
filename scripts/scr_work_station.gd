e46c82f976210af8ef7fdbdabb41ceee9eb4ae99
aherrington7452 <aherrington7452@gmail.com>
2025-02-23 20:05:39 -0500

    Cycle thru states
    set up cat animation to work with cycling through states

--- scripts/scr_work_station.gd
+++ scripts/scr_work_station.gd
@@ -2,56 +2,58 @@ extends Node2D
 
 @onready var state_label: AnimatedSprite2D = $StateLabelOutline/StateLabel
 @onready var state_label_outline: AnimatedSprite2D = $StateLabelOutline
+@onready var work_cat_animation: AnimatedSprite2D = $WorkCat
 
-enum State { Work, Coffee, Sleep }
+#dictionary
+var State = { "Work": 0, "Coffee": 1, "Sleep": 2 }
 
-var selection_state_dict = {0:"Work", 1:"Coffee", 2:"Sleep"}
-var current_state : State
-var selection_state 
+var current_state
+var current_selection_state
 
 func _ready():
-	current_state = State.Work
-	selection_state = 0
+	current_state = State["Work"]
+	current_selection_state = State["Work"]
 
 func _process(_delta) -> void:
 	if Input.is_action_just_pressed("move_up"):
-		if selection_state < 2:
-			selection_state += 1
-		elif selection_state == 2:
-			selection_state = 0
-	print("Selection State: ", selection_state)
+		if current_selection_state < 2:
+			current_selection_state += 1
+		elif current_selection_state == 2:
+			current_selection_state = 0
+	if Input.is_action_just_pressed("move_down"):
+		if current_selection_state > 0:
+			current_selection_state -= 1
+		elif current_selection_state == 0:
+			current_selection_state = 2
 	if Input.is_action_just_pressed("interact"):
-		current_state = selection_state
-	print("Current State ", current_state)
+		current_state = current_selection_state
 	label_animation()
+	cat_animation()
 
 func label_animation():
-	# TODO: if selection_state, then if current_state
-	if selection_state == 0: #work
+	if current_selection_state == State.Work:
 		state_label.play("work")
 		if current_state == State.Work:
 			state_label_outline.play("active")
 		else:
 			state_label_outline.play("inactive")
-	elif selection_state == 1: #coffee
+	elif current_selection_state == State.Coffee:
 		state_label.play("coffee")
 		if current_state == State.Coffee:
 			state_label_outline.play("active")
 		else:
 			state_label_outline.play("inactive")
-	elif selection_state == 2: #sleep
+	elif current_selection_state == State.Sleep:
 		state_label.play("sleep")
 		if current_state == State.Sleep:
 			state_label_outline.play("active")
 		else:
 			state_label_outline.play("inactive")
 
-
+func cat_animation():
+	if current_state == State.Work:
+		work_cat_animation.play("work")
 	elif current_state == State.Coffee:
-		state_label.play("default")
-		state_label.play("pressed")
-		state_label.play("default")
+		work_cat_animation.play("coffee")
 	elif current_state == State.Sleep:
-		state_label.play("default")
-		state_label.play("default")
-		state_label.play("pressed")
+		work_cat_animation.play("sleep")
